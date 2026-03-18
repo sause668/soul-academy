a26e15dab63ca993209b0877dc5281d6ac7ffe19
sause668 <soulsitescoding@zohomail.com>
2026-03-17 20:48:45 -0400

    Seed Data

---
+++ prisma/seeds/data/family-data.ts
@@ -0,0 +1,52 @@
+export const families = [
+    {
+        familyName: 'Garcia',
+        siblings: [
+            { studentId: 4 },
+            { studentId: 5 },
+        ]
+    },
+    {
+        familyName: 'Weasley',
+        siblings: [
+            { studentId: 12 },
+            { studentId: 15 },
+        ]
+    },
+    {
+        familyName: 'Stark',
+        siblings: [
+            { studentId: 13 },
+            { studentId: 14 },
+            { studentId: 16 },
+        ]
+    },
+    {
+        familyName: 'Alvarez',
+        siblings: [
+            { studentId: 17 },
+            { studentId: 19 },
+        ]
+    },
+    {
+        familyName: 'Singer',
+        siblings: [
+            { studentId: 18 },
+            { studentId: 22 },
+        ]
+    },
+    {
+        familyName: 'Smith',
+        siblings: [
+            { studentId: 27 },
+            { studentId: 35 },
+        ]
+    },
+    {
+        familyName: 'Miller',
+        siblings: [
+            { studentId: 29 },
+            { studentId: 37 },
+        ]
+    },
+]
